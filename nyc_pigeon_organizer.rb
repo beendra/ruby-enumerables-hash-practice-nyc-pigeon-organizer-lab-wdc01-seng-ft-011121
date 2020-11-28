02624c935370ecf42c7297c1ab753383286d542a
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 01:58:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,16 @@ require 'pry'
 def nyc_pigeon_organizer(data)
  # binding.pry
   final_results = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, names
-  data.each_with_object ({})
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end
+        final_array[name][key].push(inner_key)
+      end
+    end
+  end
 end
