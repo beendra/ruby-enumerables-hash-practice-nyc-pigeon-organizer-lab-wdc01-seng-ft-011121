6422c0694a59815c7476ae98f8d386341e27942e
Alexandra R <alexandra.ricardel@gmail.com>
2020-11-28 01:56:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  binding.pry
+ # binding.pry
+  final_results = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names
   data.each_with_object ({})
 end
